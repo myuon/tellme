e7dd6592cd36002f33ff546979ef01a02801bcbd
myuon <ioi.joi.koi.loi@gmail.com>
2020-03-13 02:17:12 +0900

    chore: eslint

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import React from "react";
+
+const Index: React.FC = () => (
+  <div className="root">
+    <Head>
+      <title>Tellme</title>
+    </Head>
+
+    <h1>Hello, World!</h1>
+  </div>
+);
+
+export default Index;
